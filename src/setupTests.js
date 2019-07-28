2a3bb644046295f55174352aa02037a53954f7bd
Steliyan Stoyanov <steliyan@st6.io>
2019-07-28 17:12:50 +0300

    Add @testing-library/react

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies, no-console */
 import { configure } from "enzyme";
+import { cleanup } from "@testing-library/react";
 import Adapter from "enzyme-adapter-react-16";
 
 import "jest-enzyme";
 
 configure({ adapter: new Adapter() });
+
+afterEach(() => {
+  cleanup();
+});
